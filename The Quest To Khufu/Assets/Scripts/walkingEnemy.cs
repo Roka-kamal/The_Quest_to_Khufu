d88816d05f76504cb1ad040c8c48083988bfbea0
Roka-kamal <Roaamahmoudkamal@gmail.com>
2023-12-16 01:47:01 +0200

    snake

--- The Quest To Khufu/Assets/Scripts/walkingEnemy.cs
+++ The Quest To Khufu/Assets/Scripts/walkingEnemy.cs
@@ -3,10 +3,30 @@ using System.Collections.Generic;
 using UnityEngine;
 public class walkingEnemy : Enemy_controller
 {
-    private bool canFlip = true; // Flag to control flipping
+    public float ChangeDirectionInterval; // Time interval to change direction in seconds
+    private float timeSinceLastDirectionChange;
+    private bool isMovingRight = true;
+
+    void Start()
+    {
+        timeSinceLastDirectionChange = 0f;
+    }
 
     void FixedUpdate()
     {
+        // Check if it's time to change direction
+        timeSinceLastDirectionChange += Time.deltaTime;
+        if (timeSinceLastDirectionChange >= ChangeDirectionInterval)
+        {
+            // Change direction
+            isMovingRight = !isMovingRight;
+
+            // Use the inherited Flip() method from the base class
+            Flip();
+
+            // Reset the timer
+            timeSinceLastDirectionChange = 0f;
+        }
         if (isFacingRight)
         {
             GetComponent<Rigidbody2D>().velocity = new Vector2(maxSpeed, GetComponent<Rigidbody2D>().velocity.y);
@@ -17,27 +37,6 @@ public class walkingEnemy : Enemy_controller
         }
     }
 
-    void OnTriggerEnter2D(Collider2D collider)
-    {
-        if (collider != null && canFlip)
-        {
-            if (collider.CompareTag("Wall") || collider.CompareTag("Enemy"))
-            {
-                Flip();
-                StartCoroutine(FlipCooldown());
-            }
-            else if (collider.CompareTag("Player"))
-            {
-                Debug.Log("Player collision");
-                FindObjectOfType<PlayerStats>().TakeDamage(damage);
-            }
-        }
-    }
 
-    IEnumerator FlipCooldown()
-    {
-        canFlip = false;
-        yield return new WaitForSeconds(1.0f); // Adjust the cooldown duration as needed
-        canFlip = true;
-    }
+
 }
