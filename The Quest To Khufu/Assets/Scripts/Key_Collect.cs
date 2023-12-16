4bb810ed4c72936d4e25ad07f13844de09d7ff63
Roka-kamal <Roaamahmoudkamal@gmail.com>
2023-12-16 12:07:37 +0200

    Scene UI

--- The Quest To Khufu/Assets/Scripts/Key_Collect.cs
+++ The Quest To Khufu/Assets/Scripts/Key_Collect.cs
@@ -4,26 +4,31 @@ using UnityEngine;
 
 public class Key_Collect : MonoBehaviour
 {
-    public int key_value;
+    public int keyvalue = 10;
+    public float floatSpeed = 1.0f; // Adjust the speed of the floating motion
+    public float floatMagnitude = 0.5f; // Adjust the magnitude of the floating motion
 
-    // Start is called before the first frame update
+    private Vector3 startPosition;
+
     void Start()
     {
-        
+        startPosition = transform.position;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        // Move the coin up and down using a sinusoidal function
+        float newY = startPosition.y + Mathf.Sin(Time.time * floatSpeed) * floatMagnitude;
+        transform.position = new Vector3(transform.position.x, newY, transform.position.z);
     }
 
-    void OnTriggerEnter2D (Collider2D other)
+    void OnTriggerEnter2D(Collider2D other)
     {
         if (other.tag == "Player")
         {
-          //  FindObjectOfType<player_controller>().totalkey += key_value;
+            FindObjectOfType<PlayerHealth>().keysCollected += keyvalue;
             Destroy(this.gameObject);
+
         }
     }
 }
--- The Quest To Khufu/Assets/Scripts/PlayerHealth.cs
+++ The Quest To Khufu/Assets/Scripts/PlayerHealth.cs
@@ -1,21 +1,29 @@
-using System.Diagnostics;
+using System.Collections;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class PlayerHealth : MonoBehaviour
 {
     public int maxHealth = 100;
     private int currentHealth;
-    public int coinsCollected = 0;
+    public int lives = 3;
+
+    public float flickerDuration = 0.1f; // Corrected variable name
+    private SpriteRenderer spriteRenderer;
+    public int coinsCollected;
     public int keysCollected;
     public int LettersCollected;
 
     public Slider healthSlider; // Reference to the UI Slider
+    public TextMeshProUGUI coins;
+    public TextMeshProUGUI livesLeft; // Corrected variable name
 
     void Start()
     {
-        currentHealth = maxHealth;
+        spriteRenderer = this.gameObject.GetComponent<SpriteRenderer>();
 
+        currentHealth = maxHealth;
         // Make sure you have assigned the healthSlider reference in the Inspector
         if (healthSlider == null)
         {
@@ -30,21 +38,43 @@ public class PlayerHealth : MonoBehaviour
         }
     }
 
+    void Update()
+    {
+        coins.text = ": " + coinsCollected;
+        livesLeft.text = ": " + lives;
+    }
+
     public void TakeDamage(int damage)
     {
         currentHealth -= damage;
 
-        // Update the UI health bar
-        UpdateHealthBar();
+        // Flicker the sprite when taking damage
+        SpriteFlicker();
+
         if (currentHealth <= 0)
         {
-            Die();
-        }
-        else { 
+            // Player has run out of health, decrement lives and reset health
+            lives--;
 
-        // Add any additional logic for player taking damage, like checking for death
-        UnityEngine.Debug.Log("Player took " + damage + " damage. Current Health: " + currentHealth);
-    }
+            if (lives > 0)
+            {
+                // Player has more lives, reset health
+                currentHealth = maxHealth;
+                UpdateHealthBar();
+            }
+            else
+            {
+                // No more lives, player dies
+                Die();
+            }
+        }
+        else
+        {
+            UpdateHealthBar();
+
+            // Add any additional logic for player taking damage, like checking for death
+            UnityEngine.Debug.Log("Player took " + damage + " damage. Current Health: " + currentHealth);
+        }
     }
 
     void UpdateHealthBar()
@@ -52,22 +82,42 @@ public class PlayerHealth : MonoBehaviour
         // Update the value of the health slider to reflect the player's current health
         healthSlider.value = currentHealth;
     }
+
     void Die()
     {
         // Check if the GameObject has the tag "Enemy"
-        if (gameObject.CompareTag("enime"))
+        if (gameObject.tag == "enime")
         {
             // Load the Victory scene
             GameManager.instance.gotovictoryscene();
-
         }
-        else /*(gameObject.CompareTag("Player"))*/
+        else
         {
             // For other cases, load the Game Over scene
             GameManager.instance.gotogameover();
         }
     }
 
+    void SpriteFlicker()
+    {
+        StartCoroutine(FlickerRoutine());
+    }
+
+    IEnumerator FlickerRoutine()
+    {
+        float flickerTime = 0f;
+
+        while (flickerTime < flickerDuration)
+        {
+            spriteRenderer.enabled = !spriteRenderer.enabled;
+            yield return new WaitForSeconds(0.05f); // Adjusted the waiting time
+            flickerTime += 0.05f;
+        }
+
+        spriteRenderer.enabled = true; // Ensure the sprite is visible after flickering
+    }
+
+    //collected objects
     public void CollectCoin(int coinValue)
     {
         this.coinsCollected += coinValue;
